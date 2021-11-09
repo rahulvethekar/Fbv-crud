2f6eea43b7efa8259200c59de81d1fc0484ecca2
rahulvethekar <76589326+rahulvethekar@users.noreply.github.com>
2021-11-09 13:07:16 +0530

    Update urls.py

--- fbv_crud/Student/app1/urls.py
+++ fbv_crud/Student/app1/urls.py
@@ -5,6 +5,6 @@ urlpatterns=[
     path('student/',StudentFormView,name='studentForm'),
     path('studentData/',StudentDataView,name='studentData'),
     path('studentUpdate/<int:uid>/',StudentUpdateView,name='studentUpdate'),
-    path('studentDelete/<int:did>/',StudentDeleteView,name='studentDelete')
+    path('studentDelete/<int:did>/',StudentDeleteView,name='studentDelete'),
 
 ]
